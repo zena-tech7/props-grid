efb7f620727587f0cd83b1e9ff6d2243c1a9750f
Zaina Etajer <152717751+zena-tech7@users.noreply.github.com>
2025-12-25 12:45:43 +0100

    Update App component to display employees
    Refactor App component to use Employee component and EMPLOYEES data.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import './styles.css';
-import Employees from './employees';
+import React from 'react';
+import './style.css';
+import Employee from './components/Employee';
+import { EMPLOYEES } from './data';
 
 function App() {
   return (
-    <div className="App">
-      <Employees name={employes[0].name} />
-      <Employees name={employes[1].name} />
-      <Employees name={employes[2].name} />
-      <Employees name={employes[3].name} />
+    <div className="container">
+      <h1>Notre Équipe de Choc</h1>
+      <div className="employee-grid">
+        <Employee
+          name={EMPLOYEES[0].name}
+          position={EMPLOYEES[0].position}
+          email={EMPLOYEES[0].email}
+          photo={EMPLOYEES[0].photo}
+        />
+        <Employee
+          name={EMPLOYEES[1].name}
+          position={EMPLOYEES[1].position}
+          email={EMPLOYEES[1].email}
+          photo={EMPLOYEES[1].photo}
+        />
+
+        {/* Deux derniers employés : Spread Operator */}
+        <Employee {...EMPLOYEES[2]} />
+        <Employee {...EMPLOYEES[3]} />
+      </div>
     </div>
   );
 }
